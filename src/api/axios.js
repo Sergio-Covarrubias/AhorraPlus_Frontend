e00c45cd1fbea6251a61664bc283e19769d6c1d0
Desisacc <saccescuela@gmail.com>
2024-09-23 18:38:00 -0600

    Added the backend URL as a .env variable

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import 'dotenv/config';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL: process.env.BACKEND_URL,
     withCredentials: true,
 });
 

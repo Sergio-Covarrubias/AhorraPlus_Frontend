e9ebfd750c757492f79d7dc8c83baa6f6f434669
Desisacc <saccescuela@gmail.com>
2024-09-23 19:13:29 -0600

    Revert "Added the backend URL as a .env variable"
    This reverts commit e00c45cd1fbea6251a61664bc283e19769d6c1d0.

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import 'dotenv/config';
 
 const instance = axios.create({
-    baseURL: process.env.BACKEND_URL,
+    baseURL: 'http://localhost:4000/api',
     withCredentials: true,
 });
 
